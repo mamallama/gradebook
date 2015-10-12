e2b402e9ef1b72273853369f120c1cdcb896c4fa
mamallama <reneeq25@gmail.com>
2015-10-11 21:18:02 -0400

    More fixed associations

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,8 +1,8 @@
 class Parent < ActiveRecord::Base
+  belongs_to :student
+  validates :name, presence: true
+  validates :email, presence: true
 
-    validates :name, presence: true
-    validates :email, presence: true
-
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ActiveRecord::Base
-  has_many :parent
-  has_many :grade
+  has_many :parents
+  has_many :grades
+  belongs_to :teacher
 
   validates :name, presence: true
   validates :email, presence: true
